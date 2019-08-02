83875bc0f9dbaa28cbdb25e06653ce40c1a8fb02
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-08-02 19:56:35 -0400

    needed to add a POST method to my index.handlebars and false statement to my post function

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,7 +9,7 @@ USE burgers_db;
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burger_name varchar(255) NOT NULL,
-  devoured BOOLEAN default TRUE,
+  devoured BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
 
